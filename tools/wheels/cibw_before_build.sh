8803b8558e74070d09391aab8ef15c5974ea8d14
Ralf Gommers <ralf.gommers@gmail.com>
2025-09-14 20:21:53 +0200

    native pkgconf

--- tools/wheels/cibw_before_build.sh
+++ tools/wheels/cibw_before_build.sh
@@ -40,7 +40,8 @@ if [[ "$INSTALL_OPENBLAS" = "true" ]] ; then
     # hacky) after having used the arm64 package to write out the .pc file.
     if [[ $CIBW_ARCHS_MACOS == "x86_64" ]]; then
         python -m pip install -r $PROJECT_DIR/requirements/ci_requirements.txt --platform macosx_10_13_x86_64 --only-binary :all: -U --target $(python -c "import os; print(f'{os.path.dirname(os.__file__)}/site-packages')")
-        python -m pip install pkgconf --platform macosx_10_13_x86_64 --only-binary :all: -U --target $(python -c "import os; print(f'{os.path.dirname(os.__file__)}/site-packages')")
+	# We do want the native pkgconf, since we're cross compiling
+        python -m pip install pkgconf
     fi
 
     # Copy scipy-openblas DLL's to a fixed location so we can point delvewheel
